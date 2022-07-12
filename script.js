55eb1561a855ac3d93d1db8fe3ee11c82abd04ef
Anmol Agarwal <mailanmolat@gmail.com>
2022-07-13 02:26:31 +0530

    few_updates

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrolTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -35,23 +36,76 @@ document.addEventListener('keydown', function (e) {
 
 
 
-const btnScrolTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+//Button Scrolling
+// btnScrolTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
 
-btnScrolTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-
-  //scrolling  
-  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.right + window.pageYOffset);
+//   //scrolling  
+//   // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.right + window.pageYOffset);
 
 
-  //Smooth Scrooling
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
+//   //Smooth Scrooling
+//   // window.scrollTo({
+//   //   left: s1coords.left + window.pageXOffset,
+//   //   top: s1coords.top + window.pageYOffset,
+//   //   behavior: 'smooth',
+//   // });
 
-  //Modern ways
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+//   //Modern ways
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
+
+//Page Navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     console.log('HI');
+// const id = this.getAttribute('href');
+// document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+//1. Add event listener to common parent element
+//2. Determine whar element originated the element
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  //Matching strategy
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+})
+
+
+// const h1 = document.querySelector('h1');
+// h1.addEventListener('mouseenter', function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// });
+
+// //Direct using 
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+
+///PAGE NAVIGATION
